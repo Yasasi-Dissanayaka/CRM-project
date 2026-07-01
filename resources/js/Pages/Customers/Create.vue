0cbd934fb08f70fb3a25b8ee048e9839556cb358
Yasasi <yasasianjanadissanayaka@gmail.com>
2026-07-01 11:42:43 +0530

    Customer navigation

---
+++ resources/js/Pages/Customers/Create.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="text-center">
+        <h1><b>Create Customers</b></h1><br>
+    </div>
+</template>
---
+++ resources/js/Pages/Customers/Index.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+
+defineProps({
+    customers:Array
+});
+</script>
+
+<template>
+    <div class="text-center">
+        <h1><b>Customers</b></h1><br>
+
+        <Link :href="route('customers.create')"class="bg-blue-600 text-white px-4 py-2 rounded">
+            <button style="">Add Customer</button>
+        </Link>
+        
+        <p v-for="customer in customers" :key="customer.id">
+            {{customer.name}}
+        </p>
+    </div>
+</template>
